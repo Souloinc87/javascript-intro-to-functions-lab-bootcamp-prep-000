0908a6575a23742562fc0ca9d3a6780ca20cdf79
Souloinc87 <souloinc87@gmail.com>
2017-12-31 04:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if (string === 'hello') {
   return 'I can\'t hear you!'
 }
-else if (false) {
+else if (string === 'HELLO') {
   return 'YES INDEED!'
 }
 else {
