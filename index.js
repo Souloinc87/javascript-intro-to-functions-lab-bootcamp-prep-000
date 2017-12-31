4f078dfb7feb8f62a8b60ffdb0355777dfdef036
Souloinc87 <souloinc87@gmail.com>
2017-12-31 04:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string === 'hello')
-  return "I can\'t hear you!"
+  if (string === 'hello') 
+  return lowercase
 }
 else if (string === 'HELLO') {
-  return "YES INDEED!"
+  return uppercase
+}
+else {
+  return string
 }
